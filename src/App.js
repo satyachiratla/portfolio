ca20c137ece2dea0eb32792bc14e4991cd8991c8
Vivek Chowdary <satyachiratla@gmail.com>
2021-05-25 14:48:53 +0530

    Header completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Particles from 'react-particles-js';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
+      <>
+      <Particles 
+      className="particles-canvas"
+      params={{
+        particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 900
+            }
+          },
+          shape: {
+            type: 'circle',
+            stroke: {
+              width: 6,
+              color: '#f9ab00'
+            }
+          }
+        }
+      }} />
       <Navbar />
+      <Header />
+      </>
     </div>
   );
 }
