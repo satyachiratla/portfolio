12ec7f002582598a90ad199690cd015f3fb8a40b
Vivek Chowdary <satyachiratla@gmail.com>
2021-05-25 11:09:23 +0530

    Project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
